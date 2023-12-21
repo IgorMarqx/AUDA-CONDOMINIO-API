30e2565cbf5c2d9dc5c73ecc80f0580676874abb
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-12-21 18:58:06 -0300

    WIP: implement user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'igor',
+            'email' => 'igor@gmail.com',
+            'cpf' => '12345678910',
+            'phone' => '12345678910',
+            'num_apt' => '123',
+            'password' => Hash::make('12345'),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+    }
+}
