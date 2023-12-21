cc053cf0318d3da2a951b8d00c81e3c46908a72e
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-12-21 18:54:22 -0300

    WIP: implement unauthorized and jwt security route

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\condominium\CondominiumController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::get('ping', function () {
+    return response()->json(['message' => 'pong']);
+});
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('unauthorized', function () {
+    return response()->json(['error' => 'Unauthorized'], 401);
+})->name('api.unauthorized');
+
+Route::middleware('auth:api')->group(function () {
+    Route::apiResource('condominium', CondominiumController::class);
 });
